019ff1c18be460f4f226722fd35aec8ae937de81
Aleeze <162796538+Aleeze123@users.noreply.github.com>
2024-06-04 16:48:00 +0500

    Add files via upload
    Number Guessing Game

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.underline.yellow("\n\t\t WELCOME TO NUMBER GUESSING GAME..."));
+
 
 //  1) Computer will generate a random number.
 //  2) User input for guessing number.
@@ -10,12 +13,12 @@ const answer = await inquirer.prompt([
     {
         name: "userGuessedNumber",
         type: "number",
-        message: "Please guess a number between 1-6: ",
+        message:chalk.underline.cyan( "Please guess a number between 1-6: "),
     },
 ]);
 
 if(answer.userGuessedNumber === randomNumber){
-    console.log("Congratulation You guessed right number.");
+    console.log(chalk.underline.magenta("Congratulations! You guessed right number."));
 }else{
-    console.log("You guessed wrong number");
+    console.log(chalk.underline.red("Oh no! You guessed wrong number"));
 }
